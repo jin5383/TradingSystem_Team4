37295787954f29e9c7bfa65bbaa404b4677e66e0
HongSungje <sj9120.hong@samsung.com>
2026-07-03 13:06:08 +0900

    [feature] KiwerAdapter device driver
    Signed-off-by: HongSungje <sj9120.hong@samsung.com>

--- Project14/kiwer_driver.cpp
+++ Project14/kiwer_driver.cpp
@@ -2,10 +2,23 @@
 #include "kiwer_api.cpp"
 
 class KiwerAdapter : public StockerBrocker {
-	//void login(string id, int pass) { };
-	//void buy(int code, int price, int amount) { };
-	//void sell(int code, int price, int amount) { };
-	//int  currentprice(int code) { };
+public:
+	KiwerAdapter(KiwerAPI* kiwer) {
+		m_kiwer = kiwer;
+	}
+	void login(string ID, string password) {
+		m_kiwer->login(ID, password);
+	}
+	void buy(string stockCode, int count, int price) { 
+		m_kiwer->buy(stockCode, count, price);
+	}
+	void sell(string stockCode, int count, int price) { 
+		m_kiwer->sell(stockCode, count, price);
+	}
+	int getPrice(string stockCode) {
+		return m_kiwer->currentPrice(stockCode);
+	};
 
-	// HJS
+private:
+	KiwerAPI* m_kiwer;
 };
