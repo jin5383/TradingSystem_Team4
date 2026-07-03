2a7fa74dc0d73af54ff1cbf8c3a7b35f433aa130
Hoyoung Lee <hyl4237@gmail.com>
2026-07-03 12:36:23 +0900

    [feature] test: Add selecMock Test
    Signed-off-by: Hoyoung Lee <hyl4237@gmail.com>

--- Project14/test.cpp
+++ Project14/test.cpp
@@ -1,8 +1,21 @@
 #include "nemo_api.cpp"
 #include "kiwer_api.cpp"
 #include "gmock/gmock.h"
+#include "application.cpp"
 
-TEST(Temp, Temp0)
+using namespace testing;
+using std::cout;
+
+TEST(TradingSystem, selectMock)
 {
+	TradingSystem system;
 
+	std::stringstream buffer;
+	std::streambuf* old_buffer = std::cout.rdbuf(buffer.rdbuf());
+
+	system.selectStockerBrocker(MOCK);
+
+	std::cout.rdbuf(old_buffer);
+
+	EXPECT_THAT(buffer.str(), HasSubstr("Select Mock Brocker"));
 }
