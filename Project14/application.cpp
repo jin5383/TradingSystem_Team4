1779a9ae59c121e7710e200f86d181a121948b05
Hoyoung Lee <hyl4237@gmail.com>
2026-07-03 11:08:52 +0900

    [feature] Add application
    Signed-off-by: Hoyoung Lee <hyl4237@gmail.com>

---
+++ Project14/application.cpp
@@ -0,0 +1,28 @@
+#include "stocker_brocker.cpp"
+
+class TradingSystem {
+public:
+	void selectStockerBrocker(int a) {
+		//select API
+	}
+	void login(string ID, string password) {
+
+	}
+	void buy(string stockCode, int count, int price) {
+
+	}
+	void sell(string stockCode, int count, int price) {
+
+	}
+	int getPrice(string stockCode) {
+
+	}
+	void buyNiceTiming() {
+
+	}
+	void sellNiceTining() {
+
+	}
+private:
+	StockerBrocker* stocker;
+};
--- Project14/stocker_brocker.cpp
+++ Project14/stocker_brocker.cpp
@@ -3,7 +3,6 @@ using std::string;
 #define interface struct
 
 interface StockerBrocker {
-	virtual void selectStockerBrocker();
 	virtual void login(string ID, string password);
 	virtual void buy(string stockCode, int count, int price);
 	virtual void sell(string stockCode, int count, int price);
